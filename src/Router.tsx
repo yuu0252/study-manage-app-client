ca6d488fde6dcc3e59cf4c52c95735febece8961
yuu0252 <yutanmaron@gmail.com>
2024-03-13 03:23:40 +0900

    created Login component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { AppLayout } from './components/layouts/AppLayout';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { AppLayout } from "./components/layouts/AppLayout";
+import { Login } from "./components/auth/Login";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<AppLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/chatgpt" element={<Home />} />
