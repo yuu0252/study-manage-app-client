d5549f384267f88e6bbddd06e0e801e201f1cca8
yuu0252 <yutanmaron@gmail.com>
2024-03-18 21:03:05 +0900

    change dark mode to light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import { ThemeProvider } from "@emotion/react";
 import { Router } from "./Router";
-import { CssBaseline, createTheme, useMediaQuery } from "@mui/material";
-import { useMemo } from "react";
+import { CssBaseline, createTheme } from "@mui/material";
 import { CookiesProvider } from "react-cookie";
 import { store } from "./features/store";
 import { Provider } from "react-redux";
 
 function App() {
-  const preferDarkMode = useMediaQuery("(prefers-colors-schema: dark)");
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: preferDarkMode ? "dark" : "light",
-        },
-      }),
-    [preferDarkMode]
-  );
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
 
   return (
     <CookiesProvider>
