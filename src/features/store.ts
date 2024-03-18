64e0d8bf002f4c583a183ea9ebb65ac823801c79
yuu0252 <yutanmaron@gmail.com>
2024-03-18 21:39:00 +0900

    create loading slice

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./loginSlice";
+import loadingReducer from "./loadingSlice";
 
 export const store = configureStore({
-  reducer: { login: loginReducer },
+  reducer: { login: loginReducer, loading: loadingReducer },
 });
